6f011744674680f606c0eb9f61f5ea53f1d4f1c2
GISRedeDev <dm@gisrede.com>
2024-02-04 16:45:26 +0000

    Make user model

---
+++ src/dashboard/api/app/datatypes.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class UserRoleEnum(Enum):
+    READ = "read"
+    WRITE = "write"
+    ADMIN = "admin"
---
+++ src/dashboard/api/app/models.py
@@ -0,0 +1,25 @@
+from flask_bcrypt import Bcrypt
+from sqlalchemy import Column, String, Integer, Boolean, Enum
+from sqlalchemy.ext.declarative import declarative_base
+
+import app
+
+from .datatypes import UserRoleEnum
+
+Base = declarative_base()
+bcrypt = Bcrypt(app)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(255), nullable=False)
+    role = Column(Enum(UserRoleEnum), nullable=False)
+
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = bcrypt.generate_password_hash(
+            password
+        ).decode()
+        self.role = role
