c770e24e77540568ab4c15fc953b93ffeb093eb5
alaparicioc <44085128+alaparicioc@users.noreply.github.com>
2024-02-22 15:44:48 +0000

    loglinear_model.R

--- src/analysis/toy_examples/loglinear/loglinear_model.stan.R
+++ src/analysis/toy_examples/loglinear/loglinear_model.stan.R
@@ -3,41 +3,6 @@ library("rstan")
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
-
-
-modelString <- "data {
-  int<lower=1> N;      // Number of observations
-  int<lower=1> K;      // Number of geo
-  int<lower=1, upper=K> orig[N];  
-  int<lower=1, upper=K> dest[N];  
-}
-
-parameters {
-  real alpha;  // Intercept
-  vector[K] beta1;  // Coefficients for orig
-  vector[K] beta2;  // Coefficients for dest
-}
-
-model {
-  vector[N] lambda;
-
-  for (i in 1:N) {
-    lambda[i] = alpha + beta1[orig[i]] + beta2[dest[i]];
-  }
-
-}
-
-generated quantities {
-  int<lower=0> Freq_pred[N];  // Simulated values for Freq
-
-  for (i in 1:N) {
-    Freq_pred[i] = poisson_rng(lambda[i]);   // Values from Poisson based on rate lambda
-  }
-}
-"
-
-writeLines(modelString, "src/analysis/toy_examples/loglinear/loglinear_model.stan")
-
 fit_lognormal <- stan(file = "src/analysis/toy_examples/loglinear/loglinear_model.stan",
                data = sim_data_list,
                iter = 500, 
