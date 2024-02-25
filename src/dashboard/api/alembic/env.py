79ccf53124b4ec50d0214e8454543cc494e58252
GISRedeDev <dm@gisrede.com>
2024-02-25 14:15:26 +0000

    Config to not delete spatial ref sys table

--- src/dashboard/api/alembic/env.py
+++ src/dashboard/api/alembic/env.py
@@ -65,6 +65,13 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+def include_object(object, name, type_, reflected, compare_to):
+    if type_ == 'table' and name == 'spatial_ref_sys':
+        return False
+    else:
+        return True
+
+
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
@@ -80,7 +87,7 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata, include_object=include_object
         )
 
         with context.begin_transaction():
