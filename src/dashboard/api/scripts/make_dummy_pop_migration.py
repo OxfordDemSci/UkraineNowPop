8ded1c65715ffa9f083c53976ecbcf25176a5309
GISRedeDev <dm@gisrede.com>
2024-03-03 13:53:23 +0000

    Try to optimise code and output size

--- src/dashboard/api/scripts/make_dummy_pop_migration.py
+++ src/dashboard/api/scripts/make_dummy_pop_migration.py
@@ -15,7 +15,7 @@ def divide_population(population, num_groups):
 def make_dummy_pop_data(admin_units, total_population=50000000):
     data = []
     level1_pop = divide_population(total_population, admin_units['ADM1_PCODE'].nunique())
-    days = pd.date_range(start="2024-01-01", end="2024-02-29")
+    days = pd.date_range(start="2024-01-01", end="2024-01-31")
     age_groups = np.arange(0, 85, 5)
     
     for day in days:
@@ -24,8 +24,8 @@ def make_dummy_pop_data(admin_units, total_population=50000000):
             for j, (age_min, pop2) in enumerate(zip(age_groups, age_group_pop)):
                 sex_pop = divide_population(pop2, 2)
                 for k, (sex, pop3) in enumerate(zip(['male', 'female'], sex_pop)):
-                    pop_upper = pop3 * (1 + np.random.randint(0, 20)/100)
-                    pop_lower = pop3 * (1 - np.random.randint(0, 20)/100)
+                    pop_upper = int(pop3 * (1 + np.random.randint(0, 20)/100))
+                    pop_lower = int(pop3 * (1 - np.random.randint(0, 20)/100))
                     pop_quantiles = np.random.randint(pop_lower, pop_upper + 1, 100).tolist()
                     data.append({
                         "admin_level": 1,
@@ -39,19 +39,60 @@ def make_dummy_pop_data(admin_units, total_population=50000000):
                         "pop_lower": pop_lower,
                         "pop_quantiles": pop_quantiles
                     })
+                    
+                    level2_units = admin_units[admin_units['ADM1_PCODE'] == adm1]['ADM2_PCODE'].unique()
+                    level2_pop = divide_population(pop3, len(level2_units))
+                    for l2, pop4 in zip(level2_units, level2_pop):
+                        pop_upper = int(pop4 * (1 + np.random.randint(0, 20)/100))
+                        pop_lower = int(pop4 * (1 - np.random.randint(0, 20)/100))
+                        pop_quantiles = np.random.randint(pop_lower, pop_upper + 1, 100).tolist()
+                        data.append({
+                            "admin_level": 2,
+                            "pcode": l2,
+                            "day": day,
+                            "age_min": age_min,
+                            "age_max": age_min + 5,
+                            "sex": sex,
+                            "pop": pop4,
+                            "pop_upper": pop_upper,
+                            "pop_lower": pop_lower,
+                            "pop_quantiles": pop_quantiles
+                        })
+                        
+                        level3_units = admin_units[admin_units['ADM2_PCODE'] == l2]['ADM3_PCODE'].unique()
+                        level3_pop = divide_population(pop4, len(level3_units))
+                        for l3, pop5 in zip(level3_units, level3_pop):
+                            pop_upper = int(pop5 * (1 + np.random.randint(0, 20)/100))
+                            pop_lower = int(pop5 * (1 - np.random.randint(0, 20)/100))
+                            pop_quantiles = np.random.randint(pop_lower, pop_upper + 1, 100).tolist()
+                            data.append({
+                                "admin_level": 3,
+                                "pcode": l3,
+                                "day": day,
+                                "age_min": age_min,
+                                "age_max": age_min + 5,
+                                "sex": sex,
+                                "pop": pop5,
+                                "pop_upper": pop_upper,
+                                "pop_lower": pop_lower,
+                                "pop_quantiles": pop_quantiles
+                            })
     df = pd.DataFrame(data)
+    df.insert(0, 'country', 'UKR')
+    df['admin_level'] = df['admin_level'].astype('category')
+    df['sex'] = df['sex'].astype('category')
     return df
 
 
-
-def main(adm_units: str | Path, out_csv: str | Path, pop: int = 50000000) -> None:
+def main(adm_units: str | Path, out_file: str | Path, pop: int = 50000000) -> None:
     df = pd.read_csv(adm_units)
     df_out = make_dummy_pop_data(df, pop)
-    df_out.to_csv(out_csv, index=False)
+    #df_out.to_csv(out_csv, index=False)
+    df_out.to_parquet(out_file, index=False, compression="gzip")
     
 
 
 if __name__ == "__main__":
     adm_units = BASE.joinpath("ADMIN_UNITS.csv")
-    out_csv = BASE.joinpath("dummy_pop.csv")
-    main(adm_units, out_csv)
+    out_file = BASE.parent.parent.parent.parent.joinpath("data", "dummy_tables", "dummy_pop.parquet.gzip")
+    main(adm_units, out_file)
