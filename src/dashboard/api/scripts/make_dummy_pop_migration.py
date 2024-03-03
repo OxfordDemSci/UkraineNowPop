90b88b6b2039ac06d13f5cd81ad98a63b0886099
GISRedeDev <dm@gisrede.com>
2024-03-03 15:50:59 +0000

    Make dummy migration files

--- src/dashboard/api/scripts/make_dummy_pop_migration.py
+++ src/dashboard/api/scripts/make_dummy_pop_migration.py
@@ -3,6 +3,8 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 import random
+from random import uniform
+from itertools import product
 
 BASE = Path(__file__).resolve().parent
 
@@ -77,6 +79,7 @@ def make_dummy_pop_data(admin_units, total_population=50000000):
                                 "pop_lower": pop_lower,
                                 "pop_quantiles": pop_quantiles
                             })
+        print(f"Day {day} done")
     df = pd.DataFrame(data)
     df.insert(0, 'country', 'UKR')
     df['admin_level'] = df['admin_level'].astype('category')
@@ -84,15 +87,51 @@ def make_dummy_pop_data(admin_units, total_population=50000000):
     return df
 
 
-def main(adm_units: str | Path, out_file: str | Path, pop: int = 50000000) -> None:
+def make_dummy_migration_data(df, admin_levels : list[int]):
+    days = pd.date_range(start="2024-01-01", end="2024-01-31")
+    age_groups = np.arange(0, 85, 5)
+    sexes = ['male', 'female']
+
+    data = []
+
+    for level in admin_levels:
+        admin_units = df[f"ADM{level}_PCODE"].unique().tolist()
+        for origin, destination, day, age_min, sex in product(admin_units, admin_units, days, age_groups, sexes):
+            if origin != destination:
+                probability = round(uniform(0, 1), 4)
+                data.append({
+                    "country": "UKR",
+                    "admin_level": level,
+                    "origin": origin,
+                    "destination": destination,
+                    "day": day,
+                    "age_min": age_min,
+                    "age_max": age_min + 5,
+                    "sex": 0 if sex == 'male' else 1, 
+                    "probability": probability
+                })
+    df = pd.DataFrame(data)
+    return df
+
+
+
+def main(
+        adm_units: str | Path,
+        out_file: str | Path,
+        migration_out_file: str | Path,
+        migration_levels: list[int],
+        pop: int = 50000000,
+        ) -> None:
     df = pd.read_csv(adm_units)
     df_out = make_dummy_pop_data(df, pop)
-    #df_out.to_csv(out_csv, index=False)
     df_out.to_parquet(out_file, index=False, compression="gzip")
-    
+    df_migration = make_dummy_migration_data(df, admin_levels=[1])
+    df_migration.to_parquet(migration_out_file, index=False)  
 
 
 if __name__ == "__main__":
     adm_units = BASE.joinpath("ADMIN_UNITS.csv")
     out_file = BASE.parent.parent.parent.parent.joinpath("data", "dummy_tables", "dummy_pop.parquet.gzip")
-    main(adm_units, out_file)
+    migration_out_file = BASE.parent.parent.parent.parent.joinpath("data", "dummy_tables", "dummy_migration.parquet.gzip")
+    migration_levels = [1]
+    main(adm_units, out_file, migration_out_file, migration_levels)
