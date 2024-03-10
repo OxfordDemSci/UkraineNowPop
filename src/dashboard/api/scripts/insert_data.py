6e31c67cf96863dd259f2436f15b8c98a85f263f
GISRedeDev <dm@gisrede.com>
2024-03-10 13:01:33 +0000

    Fix up dummy data and insertion

--- src/dashboard/api/scripts/insert_data.py
+++ src/dashboard/api/scripts/insert_data.py
@@ -10,17 +10,22 @@ import geopandas as gpd  # type: ignore
 from geoalchemy2 import Geometry, WKBElement
 import pandas as pd
 import fiona
+import dask
+dask.config.set({'dataframe.query-planning': True})
+import dask.dataframe as dd
 
 BASE_DIR = Path(__file__).resolve().parent.parent  # API route
 sys.path.append(str(BASE_DIR))  # API route
 
 from app.models import AdminUnits, Languages, Countries, AdminUnitsMetadata, Migration, Population
-from app.datatypes import CountriesEnum2, CountriesEnum3, UserRoleEnum
+from app.datatypes import CountriesEnum3
 from sqlalchemy.orm import sessionmaker
 from shapely.geometry import MultiPolygon
-from shapely.wkt import loads
+import logging
 
 import make_dummy_pop_migration as dummy
+
+logging.basicConfig(level=logging.INFO)
 
 ENV = BASE_DIR.parent.joinpath('.env')
 GPKG = BASE_DIR.joinpath("app", "data", "db-data", "GEODATA.gpkg")
@@ -31,6 +36,19 @@ POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_DB = os.environ.get("POSTGRES_DB")
 TABLES_DIR = os.environ.get("DATABASE_TABLES_DIR")
+CHUNK_SIZE = 20000
+
+
+def timer_and_log(func):
+    def wrapper(*args, **kwargs):
+        import time
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Time taken to run {func.__name__}: {end - start} seconds")
+        logging.info(f"Time taken to run {func.__name__}: {end - start} seconds")
+        return result
+    return wrapper
 
 try:
     conn = psycopg2.connect(
@@ -67,14 +85,15 @@ def upgrade_alembic(pg_host: str):
     command.upgrade(alembic_cfg, "head")
 
 
+@timer_and_log
 def insert_admin_units(overwrite_existing: bool = False):
     Session = sessionmaker(bind=engine)
     session = Session()
     countries = fiona.listlayers(GPKG)
     for country in countries:
-        query = session.query(AdminUnits).filter_by(country=CountriesEnum3[country]).all()
+        query = session.query(AdminUnits).filter_by(country=CountriesEnum3[country]).first()
         if query and overwrite_existing:
-            query.delete(synchronize_session=False)
+            query = session.query(AdminUnits).filter_by(country=CountriesEnum3[country]).delete(synchronize_session=False)
         if not query or overwrite_existing:
             gdf = gpd.read_file(GPKG, layer=country)
             gdf['geometry'] = gdf['geometry'].apply(
@@ -97,6 +116,7 @@ def insert_admin_units(overwrite_existing: bool = False):
     session.commit()
 
 
+@timer_and_log
 def add_languages(countries: list[dict[str, list]] = [{"UKR": ["Ukrainian", "Russian"]}]):
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -109,6 +129,7 @@ def add_languages(countries: list[dict[str, list]] = [{"UKR": ["Ukrainian", "Rus
     session.commit()
 
 
+@timer_and_log
 def add_country_access(countries: list[dict] = [{"UKR": {"National": None, "Oblast": None, "Raion": "READ", "Hromada": "READ"}}]):
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -130,89 +151,125 @@ def add_country_access(countries: list[dict] = [{"UKR": {"National": None, "Obla
                 session.add(country)
     session.commit()
 
-
+@timer_and_log
 def insert_admin_units_meta_data(overwrite_existing: bool = False):
     csv = BASE_DIR.joinpath("app", "data", "db-data", "global_pcodes.csv")
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(AdminUnitsMetadata).all()
+    query = session.query(AdminUnitsMetadata).first()
     if query and overwrite_existing:
-        query.delete(synchronize_session=False)
+        session.query(AdminUnitsMetadata).delete(synchronize_session=False)
     if not query or overwrite_existing:
-        df = pd.read_csv(csv, skiprows=[1])
-        data = []
-        for index, row in df.iterrows():
-            data.append({
-                'location': CountriesEnum3[row["Location"]],
-                'admin_level': row["Admin Level"],
-                'pcode': row["P-Code"],
-                'name': row["Name"],
-                'parent_pcode': row["Parent P-Code"],
-                'valid_from': row["Valid from date"]
-            })
-        session.bulk_insert_mappings(AdminUnitsMetadata, data)
+        for chunk in pd.read_csv(csv, skiprows=[1], chunksize=CHUNK_SIZE):
+            data = []
+            for _, row in chunk.iterrows():
+                data.append({
+                    'location': CountriesEnum3[row["Location"]],
+                    'admin_level': row["Admin Level"],
+                    'pcode': row["P-Code"],
+                    'name': row["Name"],
+                    'parent_pcode': row["Parent P-Code"],
+                    'valid_from': row["Valid from date"]
+                })
+            session.bulk_insert_mappings(AdminUnitsMetadata, data)
     session.commit()
 
-
-def add_migration_data():
-    dummy_migration = GPKG.parent.joinpath("migration.parquet.gzip")
+@timer_and_log
+def add_migration_data(overwrite_existing: bool = False):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    dummy_migration = GPKG.parent.joinpath("migration.csv")
     if not dummy_migration.exists():
-        df = dummy.main_migration(dummy_migration, [1])
-    else:
-        df = pd.read_parquet(dummy_migration)
-    df = df.astype({"admin_level": int, "age_min": int, "age_max": int, "sex": int})
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    query = session.query(Migration).all()
-    if not query:
-        data = df.to_dict(orient="records")
-        session.bulk_insert_mappings(Migration, data)
+        dummy.main_migration(dummy_migration, [1])
+    query = session.query(Migration).first()
+    if query and overwrite_existing:
+        session.query(Migration).delete(synchronize_session=False)
+    if not query or overwrite_existing:
+        for chunk in pd.read_csv(dummy_migration, chunksize=CHUNK_SIZE):
+            chunk = chunk.astype({
+                "country": "string",
+                "admin_level": "int8",
+                "origin": "string",
+                "destination": "string",
+                "age_min": "int8",
+                "age_max": "int8",
+                "sex": "int8",
+                "probability": "float32",
+                "count": "int32",
+            })
+            data = []
+            for _, row in chunk.iterrows():
+                data.append({
+                    "country": row["country"],
+                    "admin_level": row["admin_level"],
+                    "origin": row["origin"],
+                    "destination": row["destination"],
+                    "day": row["day"],
+                    "age_min": row["age_min"],
+                    "age_max": row["age_max"],
+                    "sex": row["sex"],
+                    "probability": row["probability"],
+                    "count": row["count"]
+                })
+            session.bulk_insert_mappings(Migration, data)
     session.commit()
 
-
-def add_pop_data():
-    dummy_pop = GPKG.parent.joinpath("pop.parquet.gzip")
-    if not dummy_pop.exists():
-        df = dummy.main_pop(dummy_pop)
-    else:
-        df = pd.read_parquet(dummy_pop)
-    df["sex"] = df["sex"].replace({"male": 1, "female": 2})
-    df['pop_quantiles'] = df['pop_quantiles'].apply(lambda x: [int(i) for i in x])
-    df = df.astype({
-        "country": "string",
-        "admin_level": int,
-        "pcode": "string",
-        "age_min": int,
-        "age_max": int,
-        "pop": int,
-        "pop_upper": int,
-        "pop_lower": int,
-        "sex": int,
-        })
+@timer_and_log
+def add_pop_data(overwrite_existing: bool = False):
+    def parse_pop_posterior(s):
+        parts = s.replace('[', '').replace(']', '').split(',')
+        return [int(part) for part in parts]
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(Population).all()
-    if not query:
-        data = df.to_dict(orient="records")
-        session.bulk_insert_mappings(Population, data)
+    dummy_pop = GPKG.parent.joinpath("pop.csv")
+    if not dummy_pop.exists():
+        dummy.main_pop(dummy_pop)
+    query = session.query(Population).first()
+    if query and overwrite_existing:
+        session.query(Population).delete(synchronize_session=False)
+    if not query or overwrite_existing:
+        for chunk in pd.read_csv(dummy_pop, chunksize=CHUNK_SIZE):
+            chunk["sex"] = chunk["sex"].replace({"male": 1, "female": 2})
+            chunk = chunk.astype({
+                "country": "string",
+                "admin_level": "int8",
+                "pcode": "string",
+                "age_min": "int8",
+                "age_max": "int8",
+                "pop": "int32",
+                "pop_upper": "int32",
+                "pop_lower": "int32",
+                "sex": "int8",
+            })
+            chunk['pop_posterior'] = chunk['pop_posterior'].apply(parse_pop_posterior)
+
+            data = []
+            for _, row in chunk.iterrows():
+                data.append({
+                    "country": row["country"],
+                    "admin_level": row["admin_level"],
+                    "pcode": row["pcode"],
+                    "day": row["day"],
+                    "age_min": row["age_min"],
+                    "age_max": row["age_max"],
+                    "pop": row["pop"],
+                    "pop_upper": row["pop_upper"],
+                    "pop_lower": row["pop_lower"],
+                    "sex": row["sex"],
+                    "pop_posterior": row["pop_posterior"]
+                })
+            session.bulk_insert_mappings(Population, data)
     session.commit()
 
 
 def main():
     upgrade_alembic(pg_host)
-    insert_admin_units()
-    print("Admin units inserted successfully..........................!")
+    insert_admin_units(overwrite_existing=False)
     add_languages()
-    print("Languages inserted successfully..........................!")
     add_country_access()
-    print("Country access inserted successfully..........................!")
-    insert_admin_units_meta_data()
-    print("Admin units metadata inserted successfully..........................!")
-    # add_pop_data()
-    # print("Population data inserted successfully..........................!")
-    # add_migration_data()
-    # print("Migration data inserted successfully..........................!")
-    # print("Data inserted successfully..........................!")
+    insert_admin_units_meta_data(overwrite_existing=False)
+    add_pop_data()
+    add_migration_data()
 
 
 if __name__ == "__main__":
--- src/dashboard/api/scripts/make_dummy_pop_migration.py
+++ src/dashboard/api/scripts/make_dummy_pop_migration.py
@@ -8,6 +8,8 @@ from itertools import product
 
 BASE = Path(__file__).resolve().parent
 
+UKRAINE_POPULATION = 50000000
+
 
 def divide_population(population, num_groups):
     division = np.random.multinomial(population, np.ones(num_groups)/num_groups)
@@ -17,10 +19,11 @@ def divide_population(population, num_groups):
 def make_dummy_pop_data(admin_units, total_population=50000000):
     data = []
     level1_pop = divide_population(total_population, admin_units['ADM1_PCODE'].nunique())
-    days = pd.date_range(start="2024-01-01", end="2024-01-31")
-    age_groups = np.arange(0, 85, 5)
+    days = pd.date_range(start="2024-01-01", end="2024-01-07")
+    age_groups = np.arange(0, 80, 5)
     
     for day in days:
+        print(day)
         for i, (adm1, pop1) in enumerate(zip(admin_units['ADM1_PCODE'].unique(), level1_pop)):
             age_group_pop = divide_population(pop1, len(age_groups))
             for j, (age_min, pop2) in enumerate(zip(age_groups, age_group_pop)):
@@ -28,18 +31,18 @@ def make_dummy_pop_data(admin_units, total_population=50000000):
                 for k, (sex, pop3) in enumerate(zip(['male', 'female'], sex_pop)):
                     pop_upper = int(pop3 * (1 + np.random.randint(0, 20)/100))
                     pop_lower = int(pop3 * (1 - np.random.randint(0, 20)/100))
-                    pop_quantiles = np.random.randint(pop_lower, pop_upper + 1, 100).tolist()
+                    pop_posterior = np.random.randint(pop_lower, pop_upper + 1, 1000).tolist()
                     data.append({
                         "admin_level": 1,
                         "pcode": adm1,
                         "day": day,
                         "age_min": age_min,
-                        "age_max": age_min + 5,
+                        "age_max": age_min + 4,
                         "sex": sex,
                         "pop": pop3,
                         "pop_upper": pop_upper,
                         "pop_lower": pop_lower,
-                        "pop_quantiles": pop_quantiles
+                        "pop_posterior": pop_posterior
                     })
                     
                     level2_units = admin_units[admin_units['ADM1_PCODE'] == adm1]['ADM2_PCODE'].unique()
@@ -47,18 +50,18 @@ def make_dummy_pop_data(admin_units, total_population=50000000):
                     for l2, pop4 in zip(level2_units, level2_pop):
                         pop_upper = int(pop4 * (1 + np.random.randint(0, 20)/100))
                         pop_lower = int(pop4 * (1 - np.random.randint(0, 20)/100))
-                        pop_quantiles = np.random.randint(pop_lower, pop_upper + 1, 100).tolist()
+                        pop_posterior = np.random.randint(pop_lower, pop_upper + 1, 1000).tolist()
                         data.append({
                             "admin_level": 2,
                             "pcode": l2,
                             "day": day,
                             "age_min": age_min,
-                            "age_max": age_min + 5,
+                            "age_max": age_min + 4,
                             "sex": sex,
                             "pop": pop4,
                             "pop_upper": pop_upper,
                             "pop_lower": pop_lower,
-                            "pop_quantiles": pop_quantiles
+                            "pop_posterior": pop_posterior
                         })
                         
                         level3_units = admin_units[admin_units['ADM2_PCODE'] == l2]['ADM3_PCODE'].unique()
@@ -66,18 +69,18 @@ def make_dummy_pop_data(admin_units, total_population=50000000):
                         for l3, pop5 in zip(level3_units, level3_pop):
                             pop_upper = int(pop5 * (1 + np.random.randint(0, 20)/100))
                             pop_lower = int(pop5 * (1 - np.random.randint(0, 20)/100))
-                            pop_quantiles = np.random.randint(pop_lower, pop_upper + 1, 100).tolist()
+                            pop_posterior = np.random.randint(pop_lower, pop_upper + 1, 1000).tolist()
                             data.append({
                                 "admin_level": 3,
                                 "pcode": l3,
                                 "day": day,
                                 "age_min": age_min,
-                                "age_max": age_min + 5,
+                                "age_max": age_min + 4,
                                 "sex": sex,
                                 "pop": pop5,
                                 "pop_upper": pop_upper,
                                 "pop_lower": pop_lower,
-                                "pop_quantiles": pop_quantiles
+                                "pop_posterior": pop_posterior
                             })
     df = pd.DataFrame(data)
     df.insert(0, 'country', 'UKR')
@@ -87,8 +90,8 @@ def make_dummy_pop_data(admin_units, total_population=50000000):
 
 
 def make_dummy_migration_data(df, admin_levels : list[int]):
-    days = pd.date_range(start="2024-01-01", end="2024-01-31")
-    age_groups = np.arange(0, 85, 5)
+    days = pd.date_range(start="2024-01-01", end="2024-01-07", freq="W")
+    age_groups = np.arange(0, 80, 5)
     sexes = ['male', 'female']
 
     data = []
@@ -97,7 +100,7 @@ def make_dummy_migration_data(df, admin_levels : list[int]):
         admin_units = df[f"ADM{level}_PCODE"].unique().tolist()
         for origin, destination, day, age_min, sex in product(admin_units, admin_units, days, age_groups, sexes):
             if origin != destination:
-                probability = round(uniform(0, 1), 4)
+                probability = round(uniform(0, 0.0001), 8)
                 data.append({
                     "country": "UKR",
                     "admin_level": level,
@@ -105,9 +108,10 @@ def make_dummy_migration_data(df, admin_levels : list[int]):
                     "destination": destination,
                     "day": day,
                     "age_min": age_min,
-                    "age_max": age_min + 5,
+                    "age_max": age_min + 4,
                     "sex": 0 if sex == 'male' else 1, 
-                    "probability": probability
+                    "probability": probability,
+                    "count": probability * UKRAINE_POPULATION
                 })
     df = pd.DataFrame(data)
     return df
@@ -117,25 +121,34 @@ def make_dummy_migration_data(df, admin_levels : list[int]):
 def main_migration(
         migration_out_file: str | Path,
         migration_levels: list[int],
+        save_to_parquet: bool = False
         ) -> pd.DataFrame:
     adm_units = BASE.joinpath("ADMIN_UNITS.csv")
     df = pd.read_csv(adm_units)
     df_migration = make_dummy_migration_data(df, admin_levels=migration_levels)
-    df_migration.to_parquet(migration_out_file, index=False)
-    return df_migration
+    if save_to_parquet:
+        df_migration.to_parquet(migration_out_file, index=False)
+    else:
+        df_migration.to_csv(migration_out_file, index=False)
 
 
-def main_pop(out_file: str | Path, pop: int = 50000000) -> pd.DataFrame:
+def main_pop(
+        out_file: str | Path,
+        pop: int = 50000000,
+        save_to_parquet: bool = False 
+        ) -> pd.DataFrame:
     adm_units = BASE.joinpath("ADMIN_UNITS.csv")
     df = pd.read_csv(adm_units)
     df_out = make_dummy_pop_data(df, pop)
-    df_out.to_parquet(out_file, index=False, compression="gzip")
-    return df_out
+    if save_to_parquet:
+        df_out.to_parquet(out_file, index=False, compression="gzip")
+    else:
+        df_out.to_csv(out_file, index=False)
 
 
 if __name__ == "__main__":
-    out_file = BASE.parent.parent.parent.parent.joinpath("data", "dummy_tables", "dummy_pop.parquet.gzip")
-    migration_out_file = BASE.parent.parent.parent.parent.joinpath("data", "dummy_tables", "dummy_migration.parquet.gzip")
+    out_file = BASE.parent.parent.parent.parent.joinpath("data", "dummy_tables", "dummy_pop.csv")
+    migration_out_file = BASE.parent.parent.parent.parent.joinpath("data", "dummy_tables", "dummy_migration.csv")
     migration_levels = [1]
-    _ = main_migration(migration_out_file, migration_levels)
-    _ = main_pop(out_file)
+    main_migration(migration_out_file, migration_levels)
+    main_pop(out_file)
