27dbf9d147d1b3ed0295187705cad9c25d71094c
Andrea Aparicio <44085128+alaparicioc@users.noreply.github.com>
2024-05-08 11:21:16 +0100

    Update jags_modl_multinomial.R

--- src/analysis/toy_examples/multinomial/jags_modl_multinomial.R
+++ src/analysis/toy_examples/multinomial/jags_modl_multinomial.R
@@ -1,81 +1,63 @@
 model {
-    # Observation model: N-mixture[?] model for estimating population
+    # Observation model
     for (i in 1:I) {
         for (a in 1:A) {
             for (s in 1:S) {
                 for (t in 1:T) {
-                   F[i,a,s,t] ~ dpois(exp(log_N[i,a,s,t]) * nu[i,t])
-                }}}}
+                  F[i,a,s,t] ~ dpois(exp(log_N[i,a,s,t]) * nu[i,t])
+            }}}}
     
-    # Smoothing true unobserved flows
-    # First point in time
+    # Population model[?]
     for (i in 1:I) {
         for (a in 1:A) {
             for (s in 1:S) {
-                for (t in 1:30) {
-                    log_N[i,a,s,t] ~ dnorm(u0[i], tau_N)                                          
+                for (t in 1:T) {
+                    log_N[i,a,s,t] ~ dnorm(u0[i], tau_N) 
+                    
+                    N[i,a,s,t] <- round(exp(log_N[i,a,s,t]))
                 }}}}
-                
-    # Rest 
-    for (i in 1:I) {
-        for (a in 1:A) {
-            for (s in 1:S) {
-                for (t in 31:T) {
-                    log_N[i,a,s,t] ~ dnorm(u0[i], tau_N)         
-                }}}}
-    
-                
-              
+
     # Oblast-specific intercepts for log_N
     for(i in 1:I){
         u0[i] ~ dnorm(0, 1)  # Random intercepts
-        }    
+    }
+        
+    tau_N ~ dgamma(0.01, 0.01)  # Precision for (corrected) N
+    
                 
     # Log-normal link for probability of Facebook use
     for (i in 1:I) {
         for (t in 1:T) {
-            nu[i,t] ~ dlnorm(mu_nu[i,t], tau_p)  
+            nu[i, t] <- exp(log_nu[i, t])
             
-            mu_nu[i,t] <- alpha_0 + alpha_t[t]
+            log_nu[i, t] ~ dnorm(mu_nu, tau_nu)
         }}
     
-    # Priors for parameters affecting nu[i,t]
-    alpha_0 ~ dnorm(0, 0.01)   # Prior for intercept
-    for (t in 1:T) {
-        alpha_t[t] ~ dnorm(0, 1)  # time-specific effects
-    }
+    mu_nu ~ dnorm(0, 0.001)  
+    tau_nu ~ dgamma(0.01, 0.01)
     
-  #Modelling mobility patterns  
+    # Mobility patterns
     for (i in 1:I) {
-     for (a in 1:A) {
-      for (s in 1:S) {
-       for (t in 1:(T-1)) {
-          
-          p[i,1:I,a,s,t] ~ ddirch(alpha_prior[i,1:I,a,s,t])
-                
-       for (j in 1:I) {
-          # Multinomial transitions with auxiliary data
-          N_ijast[i,j,a,s,t] ~ dmulti(p[i,j,a,s,t], round(exp(log_N[i,a,s,t]))
-     }}}}}
-                                      
-    for (i in 1:I) {
-     for (a in 1:A) {
+      for (a in 1:A) {
        for (s in 1:S) {
          for (t in 1:T) {
+                    
            for (j in 1:I) {
-             alpha_prior[i,j,a,s,t] <- exp(beta_0 + beta[j]*contiguity[i,j,a,s,t])
+             logit(p_logit[i,j,a,s,t]) <- i[i] + j[j] + a[a] + s[s] + t[t] + contiguity1[i,j]
              
-        }}}}}
-
-        beta_0 ~ dnorm(0, 1)
-        
-        for(j in 1:I){
-          beta[j] ~ dnorm(0, 1) 
+             p[i,j,a,s,t] <- exp(p_logit[i,j,a,s,t])/(1+p_logit[i,j,a,s,t])
+             }
+                    
+           # Normalisation of probabilities
+            for (j in 1:I) {
+               p_norm[i,j,a,s,t] <- p[i,j,a,s,t] / sum(p[i,1:I,a,s,t])
+       
+               lambda_ijast[i,j,a,s,t] <- exp(log_N[i,a,s,t]) * p_norm[i,j,a,s,t]
+       
+               N_ijast[i,j,a,s,t] ~ dpois(lambda_ijast[i,j,a,s,t])
+                    }
+                }
+            }
         }
-        
-        tau_mu_N ~ dgamma(0.01, 0.01)  # Precision for uncorrected N
-        tau_N ~ dgamma(0.01, 0.01)  # Precision for corrected N
-        
-        tau_p ~ dgamma(0.01, 0.01)     # Precision for scaling factor 
-    
-}
+    }
+    }
