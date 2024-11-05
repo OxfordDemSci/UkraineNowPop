a6b102cbc3f85863bf0b79326cb73f7c184a0c57
Doug Leasure <douglas.leasure@demography.ox.ac.uk>
2024-11-05 10:52:16 +0000

    improve efficiency of meta key SQL query to avoid LIMIT 100

--- src/1_pop_data/00_query_meta_keys.py
+++ src/1_pop_data/00_query_meta_keys.py
@@ -18,28 +18,31 @@ os.makedirs(out_dir, exist_ok=True)
 
 
 #---- input data ----#
-
-# agesex demographic groups
-agesex = ['T_13Plus']
-# \
-# ['F_13Plus', 'F_18Plus', 'F_20Plus', 'F_13_19', 'F_15_49', 'F_15_64', 'F_18_34', 'F_20_29', 'F_30_39', 'F_40_49', 'F_50_59', 'F_60Plus', 'F_65Plus',
-#     'M_13Plus', 'M_18Plus', 'M_20Plus', 'M_13_19', 'M_15_49', 'M_15_64', 'M_18_34', 'M_20_29', 'M_30_39', 'M_40_49', 'M_50_59', 'M_60Plus', 'M_65Plus',
-#     'T_13Plus', 'T_18Plus', 'T_20Plus', 'T_13_19', 'T_15_49', 'T_15_64', 'T_18_34', 'T_20_29', 'T_30_39', 'T_40_49', 'T_50_59', 'T_60Plus', 'T_65Plus']
-
-
-# end date
+country = 'UA'
+collection_name = 'ukraine_regions'
 date_start = '2022-02-26'
 date_end = '2023-02-25'
-
-country = 'UA'
+gender = 0
+age_min = 13
+age_max = 999
+geo_level = 'regions'
+location_type = '["recent"]'
 
 
 #---- meta key ----#
-sql="select distinct geo_name, geo_key from facebook_clean where geo_level ='regions' and location_types = '" + '["recent"]' + "' and country = 'UA' limit 100;"
+sql = (f"select distinct(geo_key), geo_name from facebook_clean where "
+       f"country = '{country}' and "
+       f"collection_name = '{collection_name}' and "
+       f"collection_date >= '{date_start}' and "
+       f"collection_date <= '{date_end}' and "
+       f"gender = {gender} and "
+       f"age_min = {age_min} and "
+       f"age_max = {age_max} and "
+       f"geo_level = '{geo_level}' and "
+       f"location_types = '{location_type}';")
 
-meta_key =pd.read_sql(sql=sql, con=pyidp.db_engine())
-
-meta_key = meta_key[-meta_key['geo_name'].isna()]
+meta_key = pd.read_sql(sql=sql, con=pyidp.db_engine())
+meta_key.dropna(inplace=True)
 
 outfile = os.path.join(out_dir, country.lower() + '_meta_keys.csv')
 meta_key.to_csv(outfile, index=False)
