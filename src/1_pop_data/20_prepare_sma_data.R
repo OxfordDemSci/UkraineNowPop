65b4d3249766d232e892062520f02444f413e46b
edarin <edith.darin@demography.ox.ac.uk>
2024-11-06 12:32:13 +0000

    remove warning on select in tidyverse

--- src/1_pop_data/20_prepare_sma_data.R
+++ src/1_pop_data/20_prepare_sma_data.R
@@ -50,12 +50,12 @@ master_index_without_t <- master_index |>
 process_sma_data <- function(sma_data, metric=audience_metric) {
 
   sma_data_ <- sma_data |>
-    rename(value = metric, i_key = meta_key) |> 
+    rename(value = {{metric}}, i_key = meta_key) |> 
     left_join(time_index )   |> 
     left_join(
       master_index_without_t )  |>
     arrange(t_name) |>
-    mutate(m = format(t_name, "%u"))|> 
+    mutate(m = format(t_name, "%u"))  |> 
     select(t_name, t, m, i, a, s, value) 
 
   return(sma_data_)
