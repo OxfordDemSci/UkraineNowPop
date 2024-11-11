14c26da41b9fdb7838acd8b06792bfea7903c8c2
edarin <edith.darin@demography.ox.ac.uk>
2024-11-11 14:41:14 +0000

    fix time_index to get all collection_date

--- src/1_pop_data/10_create_master_index.R
+++ src/1_pop_data/10_create_master_index.R
@@ -52,17 +52,18 @@ geo_index <- meta_keys |>
     i_key = as.integer(i_key)
   )
 
-time_index <- tibble(
+time_index_expanded <- tibble(
   collection_date = seq(as.Date(date_start), as.Date(date_end), by = 1)
 ) |>
   mutate(
     t_name = floor_date(as.Date(collection_date), "week", week_start = 1),
-    t_key = str_replace_all(as.character(t_name), "-", "") |> as.integer()
-  ) |>
-  select(t_name, t_key) |>
-  distinct() |>
-  arrange(t_name) |>
-  mutate(t = 1:n())
+    t_key = str_replace_all(as.character(t_name), "-", "") |> as.integer(),
+    t = t_name |> as.character()|> as_factor() |> as.numeric()
+  ) 
+
+time_index <- time_index_expanded |>
+  distinct(t_name, t_key, t) |>
+  arrange(t)
 
 agesex_index <- lapply(agesex, function(agesex_) agesex_col_to_query_args(agesex_) |> as_tibble()) |>
   bind_rows() |>
@@ -92,4 +93,4 @@ master_index <- expand_grid(
 # Write output -----------------------------------------------------------
 
 write_csv(master_index, file.path(out_dir, paste0(tolower(country), "_master_index", output_label, ".csv")))
-write_csv(time_index, file.path(out_dir, paste0(tolower(country), "_time_index", output_label, ".csv")))
+write_csv(time_index_expanded, file.path(out_dir, paste0(tolower(country), "_time_index", output_label, ".csv")))
