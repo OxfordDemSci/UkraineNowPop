dcdc520625624d65724c082ecb47bf703d2a2849
doug-leasure <douglas.leasure@demography.ox.ac.uk>
2024-11-13 13:05:57 +0000

    house keeping

--- src/1_pop_data/10_create_master_index.R
+++ src/1_pop_data/10_create_master_index.R
@@ -3,8 +3,8 @@ rm(list = ls())
 gc()
 
 # Load required helpers
-source("R_helpers/generic.R")
-source("R_helpers/data_querying.R")
+source(file.path(here::here(), "R_helpers/generic.R"))
+source(file.path(here::here(), "R_helpers/data_querying.R"))
 
 # Input parameters -------------------------------------------------------
 
@@ -59,7 +59,7 @@ time_index_expanded <- tibble(
     t_name = floor_date(as.Date(collection_date), "week", week_start = 1),
     t_key = str_replace_all(as.character(t_name), "-", "") |> as.integer(),
     t = t_name |> as.character() |> as_factor() |> as.numeric()
-  ) 
+  )
 
 time_index <- time_index_expanded |>
   distinct(t_name, t_key, t) |>
