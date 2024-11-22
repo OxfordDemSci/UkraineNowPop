b12c70ab060eb0fcde1b634d8e2983ee374487d5
edarin <edith.darin@demography.ox.ac.uk>
2024-11-22 15:59:12 +0000

    fill missing combination in acled

--- src/covariates/collection/acled.py
+++ src/covariates/collection/acled.py
@@ -119,10 +119,14 @@ if __name__ == "__main__":
             on=["ADM1_PCODE", "t"],
         )
 
-    acled_oblast = acled_oblast[
-        acled_oblast["ADM1_PCODE"].isin(master_index["ADM1_PCODE"])
-    ].merge(
-        master_index.set_index(["ADM1_PCODE", "t"]), how="right", on=["ADM1_PCODE", "t"]
+    acled_oblast = (
+        acled_oblast[acled_oblast["ADM1_PCODE"].isin(master_index["ADM1_PCODE"])]
+        .merge(
+            master_index.set_index(["ADM1_PCODE", "t"]),
+            how="right",
+            on=["ADM1_PCODE", "t"],
+        )
+        .fillna(0)
     )
 
     acled_oblast.to_csv(interim_dir / (country + "_acled_oblast.csv"), index=False)
