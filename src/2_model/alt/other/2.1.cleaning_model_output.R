c19658e931f656ad12bb79d926cadf668332db42
Andrea Aparicio <44085128+alaparicioc@users.noreply.github.com>
2025-02-03 11:33:20 +0000

    Update 2.1.cleaning_model_output.R

--- src/2_model/alt/other/2.1.cleaning_model_output.R
+++ src/2_model/alt/other/2.1.cleaning_model_output.R
@@ -18,15 +18,12 @@ pop_stocks1 <- pop_stocks %>%
      as.data.frame()
   
 pop_stocks2 <- pop_stocks1 %>%  
-  rowwise() %>%
-  mutate(across(starts_with("V"), ~ as.integer(round(.))), 
-         pop_posterior = paste0("[", paste(sort(c_across(starts_with("V"))), collapse = ","), "]")) %>% ungroup() %>% 
-      rownames_to_column(var = "parameter") %>%
-      filter(str_starts(parameter, "N")) %>%
-      mutate(parameter = str_sub(parameter, 3, -2) %>% as.integer()) %>%
-      left_join(master_index, by = "parameter") %>%  
-      separate(col = a_name, into = c("age_min0", "age_max0"), sep = "_", convert = TRUE) %>%
-      mutate(
+  rownames_to_column(var = "parameter") %>%
+  filter(str_starts(parameter, "N")) %>%
+  mutate(parameter = str_sub(parameter, 3, -2) %>% as.integer()) %>%
+  left_join(master_index, by = "parameter") %>%  
+  separate(col = a_name, into = c("age_min0", "age_max0"), sep = "_", convert = TRUE) %>%
+  mutate(
         country = rep("UKR", length(ADM1_PCODE)),
          admin_level = rep(1, length(ADM1_PCODE)) %>% as.integer(),
         age_min = ifelse(str_detect(age_min0, "Plus"), str_sub(age_min0, 1, 2), round(as.numeric(age_min0), 0)),
@@ -35,9 +32,12 @@ pop_stocks2 <- pop_stocks1 %>%
         pop = round(rowMeans(select(., starts_with("V")), na.rm = TRUE), 0) %>% as.integer(),
         pop_lower = round(rowQuantiles(as.matrix(select(., starts_with("V"))), probs = 0.025), 0) %>% as.integer(),
         pop_upper = round(rowQuantiles(as.matrix(select(., starts_with("V"))), probs = 0.975), 0) %>% as.integer()) %>%
-      rename("pcode" = "ADM1_PCODE",
-             "day" = "t_name",
-            "sex" = "s") %>%
+  rename("pcode" = "ADM1_PCODE",
+         "day" = "t_name",
+         "sex" = "s") %>%
+  rowwise() %>%
+  mutate(across(starts_with("V"), ~ as.integer(round(.))), 
+                       pop_posterior = paste0("[", paste(sort(c_across(starts_with("V"))), collapse = ","), "]")) %>% ungroup() %>% 
   dplyr::select(country, admin_level, pcode, 
                 day, 
                 age_min, age_max,
