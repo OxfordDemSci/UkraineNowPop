18752942ff8cf6b4d6c5e58813e391ee96ed7d60
Andrea Aparicio <44085128+alaparicioc@users.noreply.github.com>
2025-02-03 11:02:30 +0000

    Update 2.1.cleaning_model_output.R

--- src/2_model/alt/other/2.1.cleaning_model_output.R
+++ src/2_model/alt/other/2.1.cleaning_model_output.R
@@ -1,70 +1,53 @@
-packages <- c("tidyverse", 
-              "dotenv")
-
-if(!require(packages)) install.packages(packages)
-
-lapply(packages, library, character.only = TRUE)
-
-load_dot_env('.env')
-data_dir <- Sys.getenv('data_dir')
-
-#Loading file to clean
-load(file.path(
-  data_dir,
-  "model/coda.complN_20240702.RData"))
+library(cmdstanr)  
+library(rstan)     
+library(tidyverse)
+library(posterior)
+library(matrixStats)
 
 
-# Tidying up subnational population sizes [N_j,a,s,t]
-est_log_N <- as.data.frame(as.mcmc(do.call(rbind, coda.complN))) %>% t() %>%
-  as.data.frame() %>%
-  rownames_to_column(var = "parameter") %>%
+model_name <- "211_age_sex_model"
+
+fit <- readRDS(file.path(out_dir, model_name, "mcmc", paste0("fit_", model_name, ".rds")))
+md <- readRDS(file.path(out_dir, model_name, "mcmc", paste0("md_", model_name, ".rds")))
+
+
+pop_stocks <- fit$draws(variables = "N")
+pop_stocks1 <- pop_stocks %>%
+     as_draws_df() %>%
+     t() %>%
+     as.data.frame()
+  
+pop_stocks2 <- pop_stocks1 %>%  
   rowwise() %>%
   mutate(across(starts_with("V"), ~ as.integer(round(.))), 
          pop_posterior = paste0("[", paste(sort(c_across(starts_with("V"))), collapse = ","), "]")) %>% ungroup() %>% 
-  mutate(parameter = str_sub(parameter, 6, -2)) %>%
-  separate(col = parameter, into = c("pcode", "age_group", "sex", "day0"), sep = ",") %>%
-  mutate(country = rep("UKR", length(pcode)),
-         admin_level = rep(1, length(pcode) %>% as.integer(),
-         pcode = case_when(
-           pcode==1 ~ "UA01", pcode==2 ~ "UA05", pcode==3 ~ "UA07", 
-           pcode==4 ~ "UA12", pcode==5 ~ "UA14", pcode==6 ~ "UA18", 
-           pcode==7 ~ "UA21", pcode==8 ~ "UA23", pcode==9 ~ "UA26", 
-           pcode==10 ~ "UA32", pcode==11 ~ "UA35", pcode==12 ~ "UA44", 
-           pcode==13 ~ "UA46", pcode==14 ~ "UA48", pcode==15 ~ "UA51",
-           pcode==16 ~ "UA53", pcode==17 ~ "UA56", pcode==18 ~ "UA59", 
-           pcode==19 ~ "UA61", pcode==20 ~ "UA63", pcode==21 ~ "UA65", 
-           pcode==22 ~ "UA68", pcode==23 ~ "UA71", pcode==24 ~ "UA73", 
-           pcode==25 ~ "UA74", pcode==26 ~ "UA80", pcode==27 ~ "UA85"),  #27 oblasts
-         start_date = as.Date("2022-02-25"),
-         day0 = as.numeric(day0),
-         day = start_date + day0,
-         day = format(day, format="%d/%m/%Y"),
-         
-         sex =  as.integer(sex),
-         
-         age_group = case_when(
-           age_group==1 ~ "0-4", age_group==2 ~ "5-9", 
-           age_group==3 ~ "10-14", age_group==4 ~ "15-19", 
-           age_group==5 ~ "20-24", age_group==6 ~ "25-29", 
-           age_group==7 ~ "30-34", age_group==8 ~ "35-39", 
-           age_group==9 ~ "40-44", age_group==10 ~ "45-49", 
-           age_group==11 ~ "50-54", age_group==12 ~ "55-59", 
-           age_group==13 ~ "60-64", age_group==14 ~ "65-69",
-           age_group==15 ~ "70-74", age_group==16 ~ "75-79", 
-           age_group==17 ~ "80-999"),   #17 groups
-         
-         pop = round(rowMeans(as.matrix(est_log_N %>% dplyr::select(starts_with("V"))), na.rm = TRUE), 0) %>% as.integer(),
-         pop_lower = round(rowQuantiles(as.matrix(est_log_N %>% dplyr::select(starts_with("V"))), probs=0.025), 0) %>% as.integer(),
-         pop_upper = round(rowQuantiles(as.matrix(est_log_N %>% dplyr::select(starts_with("V"))), probs=0.975), 0) %>% as.integer())) %>%
-  separate(age_group, into = c("age_min", "age_max"), sep = "-") %>% 
-  mutate(age_min = as.integer(age_min),
-         age_max = as.integer(age_max)) %>%
-  dplyr::select(country, admin_level,
-                pcode, day, 
-                age_min, age_max,  
-                sex, pop, pop_lower, pop_upper, pop_posterior)
-
-write.csv(est_log_N, 
-          file.path(
-            data_dir,
-            "/model/cleaning_model_output/mod_output_pop.csv"))
+      rownames_to_column(var = "parameter") %>%
+      filter(str_starts(parameter, "N")) %>%
+      mutate(parameter = str_sub(parameter, 3, -2) %>% as.integer()) %>%
+      left_join(master_index, by = "parameter") %>%  
+      separate(col = a_name, into = c("age_min0", "age_max0"), sep = "_", convert = TRUE) %>%
+      mutate(
+        country = rep("UKR", length(ADM1_PCODE)),
+         admin_level = rep(1, length(ADM1_PCODE)) %>% as.integer(),
+        age_min = ifelse(str_detect(age_min0, "Plus"), str_sub(age_min0, 1, 2), round(as.numeric(age_min0), 0)),
+        age_max = ifelse(str_detect(age_min0, "Plus"), 999, round(as.numeric(age_max0), 0)),
+      
+        pop = round(rowMeans(select(., starts_with("V")), na.rm = TRUE), 0) %>% as.integer(),
+        pop_lower = round(rowQuantiles(as.matrix(select(., starts_with("V"))), probs = 0.025), 0) %>% as.integer(),
+        pop_upper = round(rowQuantiles(as.matrix(select(., starts_with("V"))), probs = 0.975), 0) %>% as.integer()) %>%
+      select(-starts_with("V")) %>%
+      rename("pcode" = "ADM1_PCODE",
+             "day" = "t_name",
+            "sex" = "s") %>%
+  dplyr::select(country, admin_level, pcode, 
+                day, 
+                age_min, age_max,
+                sex,
+                pop, pop_lower, pop_upper,
+                pop_posterior
+                )
+  
+write.csv(pop_stocks2,
+          file = file.path(out_dir, model_name, "eval", "mod_output_pop.csv"),
+          row.names = FALSE)
+                
