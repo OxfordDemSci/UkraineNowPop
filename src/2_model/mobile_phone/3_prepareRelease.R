e88ff322cce1e4369e28e179383e6fe8e0ab987f
edarin <edith.darin@demography.ox.ac.uk>
2025-07-23 21:32:50 +0100

    prepare release standardisation

---
+++ src/2_model/mobile_phone/3_prepareRelease.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+source(file.path(here::here(), "R_helpers/generic.R"))
+
+flows_hromada_agesex <- data.table::fread(file.path(out_dir, "model", "deterministic", "mobilePhone_deterministic_agesex_domestic.csv"))
+
+flows_hromada_agesex <- flows_hromada_agesex |>
+  filter(destination_oblast != "Abroad") |>
+  mutate(
+    origin_raion = ifelse(origin_raion == "Київ", "Kyiv", origin_raion),
+    destination_raion = ifelse(destination_raion == "Київ", "Kyiv", destination_raion)
+  )
+
+stocks_hromada_agesex <- flows_hromada_agesex |>
+  as_tibble() |>
+  group_by(t, a_name, s_name, hromada = destination_hromada, oblast = destination_oblast, raion = destination_raion) |>
+  summarise(
+    pop_estimated = sum(monthlyFlow_hat_calibrated),
+    .groups = "drop"
+  )
+
+
+# write output
+
+write_csv(stocks_hromada_agesex, file.path(
+  "K:/DemSci/projects/2023_WHO_Ukraine_Population/deliverables",
+  "202508 Deterministic Estimates",
+  paste0(tolower(country), "_stocks_hromada_agesex", output_label, ".csv")
+))
+
+write_csv(flows_hromada_agesex, file.path(
+  "K:/DemSci/projects/2023_WHO_Ukraine_Population/deliverables",
+  "202508 Deterministic Estimates",
+  paste0(tolower(country), "_flows_hromada_agesex", output_label, ".csv")
+))
