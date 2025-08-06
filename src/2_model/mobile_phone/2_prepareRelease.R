fe52e277bc2f4508ac7e25f009e0bbb15c698cb7
edarin <edith.darin@demography.ox.ac.uk>
2025-08-06 16:27:38 +0100

    add pcode to release

--- src/2_model/mobile_phone/2_prepareRelease.R
+++ src/2_model/mobile_phone/2_prepareRelease.R
@@ -4,19 +4,27 @@ source(file.path(here::here(), "R_helpers/generic.R"))
 dir.create(file.path(out_dir, "model", "deterministic", "deliverables", "202508"), recursive = T, showWarnings = F)
 
 
-flows_hromada_agesex <- data.table::fread(file.path(out_dir, "model", "deterministic", "mobilePhone_deterministic_agesex_domestic.csv"))
+flows_hromada_agesex <- data.table::fread(file.path(out_dir, "model", "deterministic", "mobilePhone_deterministic_agesex.csv"))
 
 flows_hromada_agesex <- flows_hromada_agesex |>
-  filter(destination_oblast != "Abroad") |>
   mutate(
-    origin_raion = ifelse(origin_raion == "Київ", "UA8000", origin_raion),
-    destination_raion = ifelse(destination_raion == "Київ", "UA8000", destination_raion),
-    destination_admin2_Pcode = str_sub(destination_raion, 1, 6),
+    origin_hromada_PCODE = ifelse(origin_hromada == "Kyiv", "UA8000000", origin_hromada),
+    origin_hromada_PCODE = ifelse(origin_hromada_PCODE != "Abroad", str_sub(origin_hromada_PCODE, 1, 9), origin_hromada_PCODE),
+    origin_raion_PCODE = ifelse(origin_raion == "Kyiv", "UA8000", origin_raion),
+    origin_raion_PCODE = ifelse(origin_hromada_PCODE != "Abroad", str_sub(origin_raion_PCODE, 1, 6), origin_raion_PCODE),
+    destination_hromada_PCODE = ifelse(destination_hromada == "Kyiv", "UA8000000", destination_hromada),
+    destination_hromada_PCODE = ifelse(destination_hromada_PCODE != "Abroad", str_sub(destination_hromada_PCODE, 1, 9), destination_hromada_PCODE),
+    destination_raion_PCODE = ifelse(destination_raion == "Kyiv", "UA8000", destination_raion),
+    destination_raion_PCODE = ifelse(destination_hromada_PCODE != "Abroad", str_sub(destination_hromada_PCODE, 1, 6), destination_raion_PCODE),
   )
+
 
 stocks_hromada_agesex <- flows_hromada_agesex |>
   as_tibble() |>
-  group_by(t, a_name, s_name, hromada = destination_hromada, oblast = destination_oblast, raion = destination_raion) |>
+  group_by(t, a_name, s_name,
+    hromada = destination_hromada, oblast = destination_oblast, raion = destination_raion,
+    hromada_PCODE = destination_hromada_PCODE, raion_PCODE = destination_raion_PCODE
+  ) |>
   summarise(
     pop_estimated = sum(monthlyFlow_hat_calibrated),
     .groups = "drop"
