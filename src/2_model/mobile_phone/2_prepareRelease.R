3a4c916f0a9d15e533c11ddd7e58e9074be05aaf
edarin <edith.darin@demography.ox.ac.uk>
2025-08-11 18:36:39 +0100

    add oblast pcode to release

--- src/2_model/mobile_phone/2_prepareRelease.R
+++ src/2_model/mobile_phone/2_prepareRelease.R
@@ -3,9 +3,15 @@ source(file.path(here::here(), "R_helpers/generic.R"))
 # create output directory
 dir.create(file.path(out_dir, "model", "deterministic", "deliverables", "202508"), recursive = T, showWarnings = F)
 
-
+# load data
+pcodes <- read_csv(file.path(here::here("src/dashboard/api/app/data/db-data/global_pcodes.csv")))
 flows_hromada_agesex <- data.table::fread(file.path(out_dir, "model", "deterministic", "mobilePhone_deterministic_agesex.csv"))
 
+pcodes <- pcodes |>
+  filter(`Admin Level` == 1 & Location == "UKR") |>
+  rename(pcode = `P-Code`)
+
+# prepare flows data
 flows_hromada_agesex <- flows_hromada_agesex |>
   mutate(
     origin_hromada_PCODE = ifelse(origin_hromada == "Kyiv", "UA8000000", origin_hromada),
@@ -23,11 +29,30 @@ flows_hromada_agesex <- flows_hromada_agesex |>
   )
 
 
+flows_hromada_agesex <- flows_hromada_agesex |>
+  left_join(
+    pcodes |>
+      select(destination_oblast = Name, destination_oblast_PCODE = pcode)
+  ) |>
+  left_join(
+    pcodes |>
+      select(origin_oblast = Name, origin_oblast_PCODE = pcode)
+  )
+
+flows_hromada_agesex <- flows_hromada_agesex |>
+  mutate(
+    origin_oblast_PCODE = ifelse(origin_oblast == "Abroad", "Abroad", origin_oblast_PCODE),
+    origin_oblast_PCODE = ifelse(origin_oblast == "Unknown", "Unknown", origin_oblast_PCODE),
+    destination_oblast_PCODE = ifelse(destination_oblast == "Abroad", "Abroad", destination_oblast_PCODE)
+  )
+
+# prepare stocks data
 stocks_hromada_agesex <- flows_hromada_agesex |>
   as_tibble() |>
   group_by(t, a_name, s_name,
     hromada = destination_hromada, oblast = destination_oblast, raion = destination_raion,
-    hromada_PCODE = destination_hromada_PCODE, raion_PCODE = destination_raion_PCODE
+    hromada_PCODE = destination_hromada_PCODE, raion_PCODE = destination_raion_PCODE,
+    oblast_PCODE = destination_oblast_PCODE
   ) |>
   summarise(
     pop_estimated = sum(pop_estimated),
