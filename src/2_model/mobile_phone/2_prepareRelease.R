95b55031f067cabd137abecef9a49fd579369fb1
edarin <edith.darin@demography.ox.ac.uk>
2025-10-06 21:55:28 +0100

    remove unwanted col in release

--- src/2_model/mobile_phone/2_prepareRelease.R
+++ src/2_model/mobile_phone/2_prepareRelease.R
@@ -5,70 +5,96 @@ library(here)
 
 source(file.path(here::here(), "R_helpers/generic.R"))
 
-output_date <- "202509"
+output_date <- "202510"
 
 # create output directory
 dir.create(file.path(out_dir, "model", "deterministic", "deliverables", output_date),
-           recursive = TRUE, showWarnings = FALSE)
+  recursive = TRUE, showWarnings = FALSE
+)
 
 # load data
 pcodes <- fread(file.path(here::here("src/dashboard/api/app/data/db-data/global_pcodes.csv")))
 flows <- fread(file.path(out_dir, "model", "deterministic", "mobilePhone_deterministic_agesex.csv"))
 
 # filter & rename pcodes
-pcodes <- pcodes[`Admin Level` == 1 & Location == "UKR",
-                 .(pcode = `P-Code`, Name)]
+pcodes <- pcodes[
+  `Admin Level` == 1 & Location == "UKR",
+  .(pcode = `P-Code`, Name)
+]
 
 # prepare flows data
 flows[, origin_hromada_PCODE :=
-         fifelse(origin_hromada == "Kyiv", "UA8000000", origin_hromada)]
-flows[origin_hromada_PCODE != "Abroad",
-      origin_hromada_PCODE := str_sub(origin_hromada_PCODE, 1, 9)]
+  fifelse(origin_hromada == "Kyiv", "UA8000000", origin_hromada)]
+flows[
+  origin_hromada_PCODE != "Abroad",
+  origin_hromada_PCODE := str_sub(origin_hromada_PCODE, 1, 9)
+]
 
 flows[, origin_raion_PCODE :=
-         fifelse(origin_raion == "Kyiv", "UA8000", origin_raion)]
-flows[origin_hromada_PCODE != "Abroad",
-      origin_raion_PCODE := str_sub(origin_raion_PCODE, 1, 6)]
+  fifelse(origin_raion == "Kyiv", "UA8000", origin_raion)]
+flows[
+  origin_hromada_PCODE != "Abroad",
+  origin_raion_PCODE := str_sub(origin_raion_PCODE, 1, 6)
+]
 
 flows[, destination_hromada_PCODE :=
-         fifelse(destination_hromada == "Kyiv", "UA8000000", destination_hromada)]
-flows[destination_hromada_PCODE != "Abroad",
-      destination_hromada_PCODE := str_sub(destination_hromada_PCODE, 1, 9)]
+  fifelse(destination_hromada == "Kyiv", "UA8000000", destination_hromada)]
+flows[
+  destination_hromada_PCODE != "Abroad",
+  destination_hromada_PCODE := str_sub(destination_hromada_PCODE, 1, 9)
+]
 
 flows[, destination_raion_PCODE :=
-         fifelse(destination_raion == "Kyiv", "UA8000", destination_raion)]
-flows[destination_hromada_PCODE != "Abroad",
-      destination_raion_PCODE := str_sub(destination_raion_PCODE, 1, 6)]
+  fifelse(destination_raion == "Kyiv", "UA8000", destination_raion)]
+flows[
+  destination_hromada_PCODE != "Abroad",
+  destination_raion_PCODE := str_sub(destination_raion_PCODE, 1, 6)
+]
 
 setnames(flows, "monthlyFlow_hat_calibrated", "pop_estimated")
 
 # merge oblast info
 setnames(pcodes, "Name", "destination_oblast")
 flows <- merge(flows, pcodes[, .(destination_oblast, destination_oblast_PCODE = pcode)],
-               by = "destination_oblast", all.x = TRUE)
+  by = "destination_oblast", all.x = TRUE
+)
 
 setnames(pcodes, "destination_oblast", "origin_oblast")
 flows <- merge(flows, pcodes[, .(origin_oblast, origin_oblast_PCODE = pcode)],
-               by = "origin_oblast", all.x = TRUE)
+  by = "origin_oblast", all.x = TRUE
+)
 
 # fix special oblast codes
-flows[origin_oblast %in% c("Abroad", "Unknown"),
-      origin_oblast_PCODE := origin_oblast]
-flows[destination_oblast == "Abroad",
-      destination_oblast_PCODE := "Abroad"]
+flows[
+  origin_oblast %in% c("Abroad", "Unknown"),
+  origin_oblast_PCODE := origin_oblast
+]
+flows[
+  destination_oblast == "Abroad",
+  destination_oblast_PCODE := "Abroad"
+]
+
+# remove unwanted column
+flows[, c("pi_hat", "scaling_factor", "monthlyFlow_hat_agesex") := NULL]
 
 # aggregate to create stocks
 stocks <- flows[, .(pop_estimated = sum(pop_estimated, na.rm = TRUE)),
-                by = .(t, a_name, s_name,
-                       hromada = destination_hromada,
-                       oblast = destination_oblast,
-                       raion = destination_raion,
-                       hromada_PCODE = destination_hromada_PCODE,
-                       raion_PCODE = destination_raion_PCODE,
-                       oblast_PCODE = destination_oblast_PCODE)]
+  by = .(t, a_name, s_name,
+    hromada = destination_hromada,
+    oblast = destination_oblast,
+    raion = destination_raion,
+    hromada_PCODE = destination_hromada_PCODE,
+    raion_PCODE = destination_raion_PCODE,
+    oblast_PCODE = destination_oblast_PCODE
+  )
+]
 
 # write output
-fwrite(stocks, file.path(out_dir, "model", "deterministic", "deliverables", output_date,
-                         paste0(tolower(country), "_stocks_hromada_agesex", output_label, ".csv")))
-fwrite(flows, file.path(out_dir, "model", "deterministic", "deliverables", output_date,
-                        paste0(tolower(country), "_flows_hromada_agesex", output_label, ".csv")))
+fwrite(stocks, file.path(
+  out_dir, "model", "deterministic", "deliverables", output_date,
+  paste0(tolower(country), "_stocks_hromada_agesex", output_label, ".csv")
+))
+fwrite(flows, file.path(
+  out_dir, "model", "deterministic", "deliverables", output_date,
+  paste0(tolower(country), "_flows_hromada_agesex", output_label, ".csv")
+))
