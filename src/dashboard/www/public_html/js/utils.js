4b61c3de0992ca00d4dcd7c2cde1949cbe66ed6c
edarin <edith.darin@demography.ox.ac.uk>
2025-12-19 17:13:38 +0000

    fix typo

--- src/dashboard/www/public_html/js/utils.js
+++ src/dashboard/www/public_html/js/utils.js
@@ -397,7 +397,7 @@ export function update_Age_Range_labele(vFirst, vLast , age_ranges_available, ag
           age_max = age_ranges_available[0][age_ranges_available[0].length - 1] + "+";
     }    
     document.getElementById('label_Age_range').innerHTML = "Ages: "+ age_min +" - "+ age_max_label;
-    document.getElementById('idAgesLabelDownloadWindow').innerHTML = age_min +" - "+     document.getElementById('label_Age_range').innerHTML = "Ages: "+ age_min +" - "+ age_max_label;
+    document.getElementById('idAgesLabelDownloadWindow').innerHTML = age_min +" - "+ age_max_label;
     ;
 }
 
